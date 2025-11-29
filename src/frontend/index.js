592f0fcf5295b3415d591d30430bb57090e344cf
Dale Peligro <ddparmstrong2021@gmail.com>
2025-11-29 04:27:58 -0800

    fixed imports and edited index.js

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,26 +1,7 @@
-// import "./styles.css";
-import Gameboard from "../backend/Gameboard.js";
-import Ship from "../backend/Ship.js";
-
-console.log("Webpack success!");
-const board = new Gameboard();
-const ship = new Ship('carrier');
-// ship.togglePlacement();
-console.log(`Ship is vertical: ${ship.getVertical()}`);
-console.log(`Ship length is: ${ship.getLength()}`);
-board.placeShip(0, 0, ship);
-board.printBoard();
-console.log('');
-board.debugShipMap();
-
-console.log('');
-console.log('attacking ship...');
-console.log('');
-console.log(`first attack returns: ${board.receiveAttack(0, 0)}`);
-console.log(`second attack returns: ${board.receiveAttack(0, 0)}`);
-console.log(`third attack returns: ${board.receiveAttack(1, 0)}`);
-board.printBoard();
-console.log('');
-board.debugShipMap();
-console.log(`current ship health: ${ship.getHealth()}`);
+import './styles.css'
+import Gameboard from '../backend/Gameboard.js'
+import Ship from '../backend/Ship.js'
+import Player from '../backend/Player.js'
+import RealPlayer from '../backend/RealPlayer.js'
+import ComputerPlayer from '../backend/ComputerPlayer.js'
 
